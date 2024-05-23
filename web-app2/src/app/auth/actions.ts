e96995f792c8f7ca8644c6305ed235629ace8b8b
Mohammad-Alismael <mhdhd260@gmail.com>
2024-05-23 13:26:34 +0400

    fix jwt auth page issue

--- web-app2/src/app/auth/actions.ts
+++ web-app2/src/app/auth/actions.ts
@@ -2,6 +2,8 @@
 
 import { createUser, findUserByEmail, findUserByUsername } from "@/lib/users";
 import bcrypt from "bcryptjs";
+import { cookies } from "next/headers";
+import { verifyJWTToken } from "@/lib/helper/route.helper";
 
 export async function signup(
   username: string,
@@ -23,3 +25,18 @@ export async function signup(
     photoURL: "",
   });
 }
+
+export const handleJwtValidation = async () => {
+  const cookie = cookies().get("jwt");
+  // if (cookie?.value && cookie?.value !== "")
+  if (!cookie || !cookie.value) return false;
+  try {
+    // Verify the JWT token
+    const payload = cookie && (await verifyJWTToken(cookie.value));
+    console.log("payload", payload?.payload.exp);
+    return true;
+  } catch (error) {
+    console.error("Error verifying JWT token auth page:", error);
+    return false;
+  }
+};
--- web-app2/src/app/auth/page.tsx
+++ web-app2/src/app/auth/page.tsx
@@ -1,19 +1,10 @@
 import AuthForm from "@/components/AuthForm";
-import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import authOptions from "@/app/api/auth/[...nextauth]/options";
-import { cookies } from "next/headers";
-import { verifyJWTToken } from "@/lib/helper/route.helper";
+import { handleJwtValidation } from "@/app/auth/actions";
 
 export default async function Home() {
-  const cookie = cookies().get("jwt");
-  try {
-    // Verify the JWT token
-    await verifyJWTToken(cookie.value);
-  } catch (error) {
-    console.error("Error verifying JWT token auth page:", error);
-  }
-  if (cookie?.value && cookie?.value !== "") redirect("/dashboard");
+  const res = await handleJwtValidation();
+  if (res) redirect("/dashboard");
 
   return (
     <main className="flex justify-center items-center w-screen h-screen">
